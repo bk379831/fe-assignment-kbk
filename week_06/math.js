eecb964f170739e0246a30bce5bec24c2d7a8a99
bk379831 <bk379831@gmail.com>
2026-05-25 00:02:34 +0900

    4-11 모듈 시스템

---
+++ week_06/math.js
@@ -0,0 +1,17 @@
+export function add(a, b) {
+    return a + b;
+}
+    
+export function sub(a, b) {
+    return a - b;
+}
+    
+export function mul(a, b) {
+    return a * b;
+}
+
+export default function () {
+    console.log("Hello, JavaScript!");
+}
+
+export const PI = 3.15;
---
+++ week_06/step11.js
@@ -0,0 +1,6 @@
+import { add, sub, mul } from "./math.js";
+import sayHello from "./math.js";
+
+console.log(add(10,20));
+console.log(sub(10,20));
+console.log(mul(10,20));
