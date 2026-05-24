0f6c27d8dc59e83947a51813fecb83b909ec5296
bk379831 <bk379831@gmail.com>
2026-05-24 23:17:08 +0900

    4-5 조건문

---
+++ week_06/step05.js
@@ -0,0 +1,36 @@
+let score = 80;
+
+// if (score >= 90) {
+// console. log("90점이 넘었습니다.");
+// } else {
+// console. log("90점이 넘지 않았습니다.");
+
+// 100 ~ 90: A
+// 89 ~ 80: B
+// 79 ~70: C
+// 69 ~ : D
+
+// if (score >= 90) {
+// console.log("A!!");
+// } else if (score >= 80) {
+// console.log("B!!");
+// } else if (score >= 70) {
+//   console.log("C!!");
+// } else {
+//   console.log("D!!");
+// }
+
+let day = "Friday";
+
+switch (day) {
+    case "Monday":
+        console.log("월요일 입니다.");
+        break;
+
+    case "Friday":
+        console.log("금요일 입니다.");
+        break;
+
+    default:
+        console.log("아무 요일도 아닙니다.");
+}
