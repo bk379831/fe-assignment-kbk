1612a5b602ff6507d01a5aaaf3c505ba3c31ba61
bk379831 <bk379831@gmail.com>
2026-05-24 23:26:17 +0900

    4-6 반복문

---
+++ week_06/step06.js
@@ -0,0 +1,48 @@
+// for (초기값 ; 조건 ; 증가값) { 실행할 코드 }
+// (1) 초기값
+// (2) 조건
+// (3) 실행할 코드
+// (4) 증가값 > 2번
+
+// for (let i = 0; i <5; i++) {
+//     i => 0, 코드 실행
+//     i => 1, 코드 실행
+//     i => 2, 코드 실행
+//     i => 3, 코드 실행
+//     i => 4, 코드 실행
+//     i => 5, 코드 실행
+//     console.log("Hello!");
+// }
+
+// const LEN = 10;
+
+// for (let i = 0; i < LEN; i++) {
+//     console.log(i);
+// }
+
+// for (let i= 0; i < 10; i++) {
+//     if (i === 5) {
+//         break;
+//     }
+    
+//     console.log(i);
+// }
+
+// for (let i = 0;i < 10; ++i) {
+//     if (i % 2 === 0) {
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+// while (조건) { 실행할 코드 }
+let i = 0;
+while (i < 10) {
+    i++;
+
+    if (i % 2 === 0) {
+        continue;
+    }
+
+    console.log(i);
+}
