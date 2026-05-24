c781993a15b1a589d593648a78f525580f373d9e
bk379831 <bk379831@gmail.com>
2026-05-24 22:56:19 +0900

    4-3 자료형

---
+++ week_06/step03.js
@@ -0,0 +1,27 @@
+// (1) Number Type
+let num1 = 42;
+let num2 = 3.14;
+
+let num3 = Infinity;
+let num4 = -Infinity;
+let num5 = "hello" / 2; //NaN;
+
+// (2) String Type
+let str1 = "Hello";
+
+// prettier-ignore
+let str2 = 'World';
+
+let str3 = 'Hello, ${num2} World, ${num1}';
+console.log(str3);
+// (3) Boolean Type
+let isStudent = true;
+let isAdult = false;
+
+// (4) Undefined Type
+let user;
+
+console.log(user);
+
+// (5) Null Type
+let user2 = null;
