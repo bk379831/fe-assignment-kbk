d0700f8ae0ea299874af241952aa95da5cfcb72d
bk379831 <bk379831@gmail.com>
2026-05-24 22:48:41 +0900

    4-2 변수와 상수

---
+++ week_06/step02.js
@@ -0,0 +1,35 @@
+ let user = "david";
+
+ console.log(user);
+
+ const birth = "2010.01.01";
+
+ console.log(birth);
+
+ user = "alex";
+
+ console.log(user);
+
+ // birth = "2000.01.01";
+
+ // let user = "alex";
+ // let const = "2010.01.01";
+
+ // (1) 변수 이름에 들어갈 특수 문자는 _, $만 가능
+ let _user = "miso";
+ let $user = "hello";
+
+ // (2) 변수 이름에 숫자는 앞에 올 수 없다.
+ // let 2user = 'hello2';
+ let user2 = "hello2";
+
+ // (3) 자바스크립트에서 사용하는 예약어는 변수 이름으로 사용할 수 없다.
+ // let const = 'hello2';
+
+ // (4) 변수 이름은 의미가 있어야 한다.
+ // let x = 25;
+ let age = 25;
+
+ // (5) 변수 이름은 카멜케이스
+ let userLastName = 'so';
+ let userFirstName = 'so';
