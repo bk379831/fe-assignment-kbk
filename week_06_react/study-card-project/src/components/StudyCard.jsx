f2c32fc274489c6b6f5045d3da6abe598349c13e
bk379831 <bk379831@gmail.com>
2026-05-26 20:03:34 +0900

    icon 수정

--- week_06_react/study-card-project/src/components/StudyCard.jsx
+++ week_06_react/study-card-project/src/components/StudyCard.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
 import "./StudyCard.css";
+import starIcon from "../assets/star.svg";
 
 const studyList = [
   {
     step: "1강",
     title: "리액트 소개",
     time: "6:41",
-    level: "난이도 ⭐",
+    level: 1,
     description:
       "React가 무엇인지 알아보고, 컴포넌트 기반으로 화면을 만드는 방식을 이해합니다.",
     tag: "React",
@@ -15,7 +16,7 @@ const studyList = [
     step: "2강",
     title: "Node.js 설치하기",
     time: "3:55",
-    level: "난이도 ⭐",
+    level: 1,
     description:
       "React 프로젝트를 실행하기 위해 필요한 Node.js와 npm의 역할을 익힙니다.",
     tag: "Node.js",
@@ -24,7 +25,7 @@ const studyList = [
     step: "3강",
     title: "첫 리액트 프로젝트 만들기",
     time: "10:10",
-    level: "난이도 ⭐⭐",
+    level: 2,
     description:
       "Vite를 사용해서 React 프로젝트를 만들고 실행하는 방법을 연습합니다.",
     tag: "Vite",
@@ -33,7 +34,7 @@ const studyList = [
     step: "4강",
     title: "JSX 문법 이해하기",
     time: "13:02",
-    level: "난이도 ⭐⭐",
+    level: 2,
     description:
       "HTML처럼 보이지만 JavaScript 안에서 작성하는 JSX 문법을 이해합니다.",
     tag: "JSX",
@@ -42,7 +43,7 @@ const studyList = [
     step: "5강",
     title: "컴포넌트 이해하기",
     time: "14:34",
-    level: "난이도 ⭐⭐",
+    level: 2,
     description:
       "화면을 작은 조각으로 나누고 재사용하는 컴포넌트 개념을 학습합니다.",
     tag: "Component",
@@ -51,7 +52,7 @@ const studyList = [
     step: "6강",
     title: "리액트에서 CSS 사용하기",
     time: "10:49",
-    level: "난이도 ⭐",
+    level: 1,
     description:
       "className과 CSS 파일 import를 이용해 React 컴포넌트에 스타일을 적용합니다.",
     tag: "CSS",
@@ -60,7 +61,7 @@ const studyList = [
     step: "7강",
     title: "리액트 컴포넌트 만들고 스타일링하기",
     time: "실습과제",
-    level: "난이도 ⭐⭐⭐",
+    level: 3,
     description:
       "배운 내용을 활용해서 직접 React 컴포넌트를 만들고 CSS로 스타일링합니다.",
     tag: "Project",
@@ -90,7 +91,20 @@ function StudyCard() {
 
       <h1 className="study-card__title">{currentStudy.title}</h1>
 
-      <p className="study-card__level">{currentStudy.level}</p>
+      <div className="study-card__level">
+        <span>난이도</span>
+
+        <div className="study-card__stars">
+          {Array.from({ length: currentStudy.level }).map((_, index) => (
+            <img
+              key={index}
+              src={starIcon}
+              alt="별"
+              className="study-card__star"
+            />
+          ))}
+        </div>
+      </div>
 
       <p className="study-card__description">{currentStudy.description}</p>
 
